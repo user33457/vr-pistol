b5800268fc1301a8a52c97685b54c11efdc74396
user33457 <i.dudchenko1022012@gmail.com>
2024-10-17 21:56:07 +0200

    dobavili strelbu zakonchili

--- Assets/Gun Pack/Scripts/Bullet.cs
+++ Assets/Gun Pack/Scripts/Bullet.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] int impulseForBarrel;
+    private Rigidbody rb;
     void Start()
     {
-        //gameObject.transform.
+        rb.AddForce(transform.forward * impulseForBarrel, ForceMode.Impulse);
     }
 
     // Update is called once per frame
--- Assets/Scripts/Weapons.cs
+++ Assets/Scripts/Weapons.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Weapons : MonoBehaviour
 {
     [SerializeField] Transform barrel;
+    [SerializeField] PullZatvor bulletPool;
+    private Animator animator;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
     public void Fire() 
     {
-        RaycastHit hit;
-        if(Physics.Raycast(barrel.transform.position, barrel.transform.forward, out hit)) 
-        {
-            Target target = hit.transform.GetComponent<Target>();
-            if (target != null)
-            { 
-                Destroy(target.gameObject); 
-            }
-        }
+        animator.SetTrigger("fire");
+        GameObject bullet = bulletPool.GetBullet();
+        bullet.transform.position = barrel.transform.position;
+        bullet.transform.rotation = barrel.transform.rotation;
+        bullet.SetActive(true);
+        //RaycastHit hit;
+        //if(Physics.Raycast(barrel.transform.position, barrel.transform.forward, out hit)) 
+        //{
+        //    Target target = hit.transform.GetComponent<Target>();
+        //    if (target != null)
+        //    { 
+        //        Destroy(target.gameObject); 
+        //    }
+        //}
     }
 }
